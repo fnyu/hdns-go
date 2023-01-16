be07f2e07bf26802f28d470fbb298424830dc7b3
Jakub K <22723539+fnyu@users.noreply.github.com>
2023-01-16 16:05:29 +0000

    Initialize module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fnyu/hdns-go
+
+go 1.19
---
+++ hdns/hdns.go
@@ -0,0 +1,8 @@
+// Package hdns-go is a library for interacting with Hetzner DNS API.
+//
+// Some functions were copy-pasted directly from the following official
+// Hetzner Cloud repository:
+//   - https://github.com/hetznercloud/hcloud-go
+package hdnsapi
+
+const Version = "0.0.1"
